6f671af710c5b6199f9824a58bbe4d2bed037473
CesarTecun <ctecunl1@miumg.edu.gt>
2025-05-24 01:03:40 -0600

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/', // 👈 esto permite que las rutas funcionen en producción
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
